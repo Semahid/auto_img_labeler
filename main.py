6fdb53d1a2bc90f0f795a0a90122db0d81ee1187
halak0013 <zumruduanka0013@gmail.com>
2025-03-31 16:40:28 +0300

    Add build directory to .gitignore and set application window icon in main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication
 import sys
 from src.ui.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("data/icon.png"))
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
